ed3fd6e1f8a00f45d35a6c8e352dc8bc3505ab7e
Michael Mattingly <mikeamatt1@hotmail.com>
2023-09-04 13:58:04 -0400

    pushing server and client to main

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,85 @@
+import { GraphQLError } from "graphql";
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select("-__v -password")
+          .populate("books");
+
+        return userData;
+      }
+
+      throw new GraphQLError("You must be logged in!", {
+        extensions: {
+          code: "UNAUTHENTICATED",
+        },
+      });
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new GraphQLError("User Not Found", {
+          extensions: {
+            code: "UNAUTHENTICATED",
+          },
+        });
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new GraphQLError("Incorrect Password", {
+          extensions: {
+            code: "UNAUTHENTICATED",
+          },
+        });
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, { input }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: input } },
+          { new: true }
+        );
+
+        return updatedUser;
+      }
+      throw new GraphQLError("You must be logged in!", {
+        extensions: {
+          code: "UNAUTHENTICATED",
+        },
+      });
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId: bookId } } },
+          { new: true }
+        );
+        throw new GraphQLError("You must be logged in!", {
+          extensions: {
+            code: "UNAUTHENTICATED",
+          },
+        });
+      }
+    },
+  },
+};
+
+module.exports = resolvers;
