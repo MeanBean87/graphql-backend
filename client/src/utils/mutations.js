bceb993f9d43528e4a032c5dc707890169021022
Michael Mattingly <mikeamatt1@hotmail.com>
2023-09-04 12:30:05 -0400

    added correct npm packages, built queries and mutations on client

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,65 @@
+import { gpl } from '@apollo/client';
+
+export const ADD_USER = gpl`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password){
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
+
+export const LOGIN_USER = gpl`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password){
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gpl`
+    mutation saveBook($input: savedBook!) {
+        saveBook(input: $input){
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gpl`
+    mutation removeBook($bookId: ID!) {
+        removeBook(bookId: $bookId){
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
---
+++ client/src/utils/queries.js
@@ -0,0 +1,21 @@
+import { gpl } from '@apollo/client';
+
+export const QUERY_ME = gpl`
+    query me {
+        me {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
+
